e6d6b2bef67c30e954f3e7e4c5f5f38b66992129
alpha <nuruddinsayeed@gmail.com>
2020-08-05 21:37:15 +0600

    Qustion Added

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from .models import Question, Choice
 
-# Create your views here.
+
+def index(request):
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list": latest_question_list}
+
+    return render(request, "polls/index.html", context)
+
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question Does Not Exist")
+    return render(request, "poll/results.html", {"question": question})
+
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+
+    return render(request, "polls/results.html", {"question": question})
