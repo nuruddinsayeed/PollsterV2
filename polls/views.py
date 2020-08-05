38a5b9d57880f25227d4aa2e1b2880532abda350
alpha <nuruddinsayeed@gmail.com>
2020-08-06 03:44:34 +0600

    Initial site completed

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.template import loader
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+
 from .models import Question, Choice
 
 
@@ -21,3 +25,17 @@ def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
 
     return render(request, "polls/results.html", {"question": question})
+
+
+def vote(request, question_id):
+    # print(request.POST['choice'])
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay Quesiton Voting Form
+        return render(request, "polls:detail.html", {"question": question, "error_message": "you didn't select a choice"})
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question_id, )))
